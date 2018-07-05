1fedf33615e2396fb9f3dcad063b7e1a3ef8be50
passkwall <apcipollone3@gmail.com>
2018-07-04 22:00:12 -0700

    new messages on create thing

--- server/server.js
+++ server/server.js
@@ -14,11 +14,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user has connected');
 
-    socket.on('disconnect', () => {
-        console.log('User has disconnected');
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
+
+    // socket.emit('newEmail', {
+    //     from: 'pasquale@pasquale.net',
+    //     text:'oh hey what up'
+    // });
+    //
+    // socket.on('createEmail', (data) => {
+    //     console.log('createEmail', data);
+    // });
+    //
+    // socket.on('disconnect', () => {
+    //     console.log('User has disconnected');
+    // });
 });
 
 server.listen(port, () => {
     console.log(`Started up on port ${port}`);
 });
+
+//newMessage event from server - from, text, date - log to console
+//createMessage event from client to server - from, text
