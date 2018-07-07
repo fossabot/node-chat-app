18e9444e65dc284f8476b7477561d73049704772
passkwall <apcipollone3@gmail.com>
2018-07-06 21:30:04 -0700

    add jquery and some forms

--- public/js/index.js
+++ public/js/index.js
@@ -20,8 +20,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New Message:', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
-// socket.on('newEmail', function (email) {
-//     console.log('New Email', email);
-// });
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
+});
